73d0c634093b8dc28f4909f86ec0a40507d5ce79
akash8103 <106794908+akash8103@users.noreply.github.com>
2022-07-10 23:32:58 +0530

    Add files via upload

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect, Response
 from functools import wraps
-import json
+from json import dumps
 import pymongo
 
 app = Flask(__name__)
@@ -31,12 +31,45 @@ def home():
 @app.route('/dashboard/')
 @login_required
 def dashboard():
-  return render_template('index.html')
+  try:
+    tusers = db.gvpbot.count_documents({})
+    male = db.gvpbot.count_documents({"Gender":"MALE"})
+    female = db.gvpbot.count_documents({"Gender":"FEMALE"})
+    # english = db.gvpbot.sum({"$English_Language"})
+    english = db.gvpbot.count_documents({"English_Language":1})  
+    hindi = db.gvpbot.count_documents({"Hindi_Language":1})
+    gujarati = db.gvpbot.count_documents({"Gujarati_Language":1})
+    return render_template('index.html', tusers=tusers, male=male, female=female, english=english, hindi=hindi, gujarati=gujarati)
+    #return dumps(names)
+  except Exception as e:
+    return dumps({'error' : str(e)})
+  # return render_template('index.html')
 
 #Tables Page
 @app.route('/tables', methods=["GET"])
 def tables():
-    return render_template("tables.html")
+  try:
+    botusers = []
+
+    tbl="<thead><tr><th>Name</th><th>Email ID</th><th>Gender</th><th>Address</th><th>Contact No</th></tr></thead>"
+    botusers.append(tbl)
+
+    for y in db.gvpbot.find():
+      fname = "%s"%y['fName']
+      botusers.append(fname)
+      lname = "%s"%y['lName']
+      botusers.append(lname)
+      # email = "<td>%s</td>"%y['Email_ID']
+      # botusers.append(email)
+      gender = "%s"%y['Gender']
+      botusers.append(gender)
+      address = "%s"%y["Address"]
+      botusers.append(address)
+      # mobile = "%s"%y['Mobile_No']
+      # botusers.append(mobile)
+    return render_template("tables.html", fname=fname, lname=lname, gender=gender, address=address)
+  except Exception as e:
+    return dumps({'error' : str(e)})
 
 #Charts Page
 @app.route('/charts', methods=["GET"])
@@ -47,3 +80,4 @@ def charts():
 @app.route('/profile', methods=["GET"])
 def profile():
     return render_template("profile.html")
+
